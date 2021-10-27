a9fb63eb13473e1bc611f2823740db319f53451c
Ali Aydemir <ali.aydemir93@hotmail.com>
2021-10-27 10:59:35 +0300

    404 page

--- pages/_error.js
+++ pages/_error.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import Layout from '../src/components/Layout/Layout';
-
+import Main from '../src/components/MainHOC/Main';
+import Image from "next/image";
+import Link from 'next/link';
+import logo from "../public/logo-main.svg"
 function Error({ statusCode }) {
     return (
-        <p>
-            {statusCode}
-        </p>
+        <Main>
+            <div className="page404">
+                <div className="page404__container">
+                    <div className="page404__logo-box">
+                        <Image src={logo}
+                            layout="fill"
+                            alt="Logo main"
+                            unoptimized="true"
+                            objectFit="contain"
+                        />
+                    </div>
+                    <p className="page404__status">
+                        {statusCode} Sayfa Bulunamadı
+                    </p>
+                </div>
+
+                <div>
+                    <Link href="/" as="/">
+                        <a className="page404__link">Anasayfaya Git</a>
+                    </Link>
+                </div>
+            </div>
+
+        </Main>
     )
 }
 
